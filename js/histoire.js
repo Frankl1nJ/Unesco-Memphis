aac0cb8ed357e98dfc2cda7556e3c194ff3416fe
JulienS-Code <julien.synaeve@gmail.com>
2024-11-05 19:16:48 +0100

    Upload new version (clone of Galaktik's repo)

--- js/histoire.js
+++ js/histoire.js
@@ -1,26 +1,77 @@
-const allRonds = document.querySelectorAll('.rond');
-const allBoxes = document.querySelectorAll('.box');
+function afficherTexteCompletFr() {
+    var moreText = document.getElementById("moreText");
+    var btn = document.getElementById("lirePlusBtn");
 
-const controller = new ScrollMagic.Controller()
-
-allBoxes.forEach(box => {
-
-    for(i = 0; i < allRonds.length; i++){
-
-        if(allRonds[i].getAttribute('data-anim') === box.getAttribute('data-anim')){
-
-            let tween = gsap.from(box, {y: -50, opacity: 0, duration: 0.5})
-
-            let scene = new ScrollMagic.Scene({
-                triggerElement: allRonds[i],
-                reverse: true
-            })
-            .setTween(tween)
-            // .addIndicators()
-            .addTo(controller)
-
-        }
-
+    if (moreText.style.display === "none" || moreText.style.display === "") {
+        moreText.style.display = "block";
+        btn.innerHTML = "Lire moins";
+    } else {
+        moreText.style.display = "none";
+        btn.innerHTML = "Lire plus";
     }
+}
 
-})
+function afficherTexteCompletEn() {
+    var moreText = document.getElementById("moreText");
+    var btn = document.getElementById("lirePlusBtn");
+
+    if (moreText.style.display === "none" || moreText.style.display === "") {
+        moreText.style.display = "block";
+        btn.innerHTML = "Read less";
+    } else {
+        moreText.style.display = "none";
+        btn.innerHTML = "Read more";
+    }
+}
+
+function afficherTexteCompletCn() {
+    var moreText = document.getElementById("moreText");
+    var btn = document.getElementById("lirePlusBtn");
+
+    if (moreText.style.display === "none" || moreText.style.display === "") {
+        moreText.style.display = "block";
+        btn.innerHTML = "阅读更少";
+    } else {
+        moreText.style.display = "none";
+        btn.innerHTML = "阅读更多";
+    }
+}
+
+var slideIndex = 1;
+
+function showSlides(n) {
+var i;
+var slides = document.getElementsByClassName("custom-slider");
+var dots = document.getElementsByClassName("dot");
+var texts = document.querySelectorAll('.slide-text .text-item');
+
+if (n > slides.length) {
+slideIndex = 1
+}
+if (n < 1) {
+slideIndex = slides.length
+}
+for (i = 0; i < slides.length; i++) {
+slides[i].style.display = "none";
+}
+for (i = 0; i < dots.length; i++) {
+dots[i].className = dots[i].className.replace(" active", "");
+}
+for (i = 0; i < texts.length; i++) {
+texts[i].classList.remove("active");
+}
+slides[slideIndex - 1].style.display = "block";
+dots[slideIndex - 1].className += " active";
+texts[slideIndex - 1].classList.add("active");
+}
+
+function plusSlides(n) {
+showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+showSlides(slideIndex = n);
+}
+
+showSlides(slideIndex);
+
